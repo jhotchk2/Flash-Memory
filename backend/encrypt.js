f0c4fc9ed15ef171fe96889ee3c7530bfdf41e4b
jhotchk2 <jhotchk2@emich.edu>
2024-09-21 16:32:55 -0400

    started working on encryption endpoint

---
+++ backend/encrypt.js
@@ -0,0 +1,3 @@
+export function encrypt(file) {
+  // this function will encrypt the file and return a string variable with the ciphertext
+}
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors'
+import fs from 'fs'
+import { encrypt } from './encrypt.js'
+
 const app = express()
 const PORT = 8080
 
@@ -8,6 +11,18 @@ app.use(cors())
 
 app.get('/test', (req, res) => {
   res.send('hello world')
+})
+
+app.post('/encrypt', (req, res) => {
+  // const ciphertext = encrypt()
+  // fs.writeFile('/Volumes/Untitled/test.txt', ciphertext, err => {
+  //   if (err) {
+  //     console.error(err);
+  //   } else {
+  //     console.log('file created!')
+  //   }
+  // });
+  console.log(req.body)
 })
 
 app.listen(PORT, () => {
