3f6cd0b2c1cb302175d830dc5a2405c7a51e2ec0
jhotchk2 <jhotchk2@emich.edu>
2024-09-24 15:41:21 -0400

    added dot files for keys and passed them to encrypt function

--- backend/encrypt.js
+++ backend/encrypt.js
@@ -1,4 +1,6 @@
-export function encrypt(plaintext, key) {
+export function encrypt(plaintext, keys) {
   // this function will encrypt the file and return a string variable with the ciphertext
+  // the plaintext parameter is a string of the plain text
+  // the keys parameter is an array with all of the keys
   return `${plaintext} this is from the backend`
 }
--- backend/index.js
+++ backend/index.js
@@ -10,24 +10,48 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/test', (req, res) => {
-  const name = req.body
-  console.log(name)
+  res.send('hello')
 })
 
-app.post('/encrypt', (req, res) => {
+app.post('/encrypt', async (req, res) => {
   const plaintext = req.body.plaintext
-  const key = req.body.key
   const filename = req.body.filename
-  const ciphertext = encrypt(plaintext, key)
+  const keys = getKeys()
+
+  const ciphertext = encrypt(plaintext, keys)
   fs.writeFile(`/Volumes/Untitled/${filename}`, ciphertext, err => {
     if (err) {
       console.error(err);
     } else {
       console.log('file created!')
     }
-  });
+  })
+})
+
+app.get('/decrypt', (req, res) => {
+  
 })
 
 app.listen(PORT, () => {
   console.log(`Sever running on port ${PORT}`)
 })
+
+function getKeys() {
+  let key = ''
+  let ciphers = []
+
+  try {
+    key = fs.readFileSync('/Volumes/Untitled/.key.txt', 'utf8')
+  } catch (err) {
+    console.log(err)
+  }
+
+  try {
+    const data = fs.readFileSync('/Volumes/Untitled/.cipher.txt', 'utf8')
+    ciphers = data.split('\n')
+  } catch (err) {
+    console.log(err)
+  }
+
+  return [key, ...ciphers]
+}
